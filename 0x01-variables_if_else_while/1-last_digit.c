126e2c7163e8c498e7de5b0c1e64f022e5b8e72a
liyu <liyuethiop@gmail.com>
2022-09-09 07:34:34 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,11 +24,11 @@ int main(void)
 
 	printf("Last digit of %d is %d ", n, lastdigit);
 	if (lastdigit > 5)
-		printf("and is greater than 5");
+		printf("and is greater than 5 ");
 	else if (lastdigit == 0)
-		printf("and is 0");
+		printf("and is 0 ");
 	else
-		printf("and is less than 6 and not 0");
+		printf("and is less than 6 and not 0 ");
 	return (0);
 
 }

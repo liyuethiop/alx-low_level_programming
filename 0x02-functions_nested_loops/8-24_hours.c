4df329c8b1a6fefcd93d460eaa4368a71963b607
liyu <liyuethiop@gmail.com>
2022-09-13 10:40:40 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * jack_bauer - is the fun
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int min;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar('0' + hour);
+			_putchar(':');
+			_putchar('0' + min);
+		}
+	}
+}

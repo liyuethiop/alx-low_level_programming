a4445427faeec2fa07fea9789ad4a0991c6fa1b4
liyu <liyuethiop@gmail.com>
2022-09-13 13:50:58 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,18 +8,21 @@ void print_to_98(int n)
 {
 	while (n <= 98)
 	{
-		if (n / 10 == 0)
-		{
+		if (n == 98)
 			_putchar('0' + n);
+		else if (n / 10 == 0)
+		{
 			_putchar(',');
 			_putchar(' ');
+			_putchar('0' + n);
+
 		}
 		else
 		{
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
 			_putchar(',');
 			_putchar(' ');
+			_putchar('0' + n / 10);
+			_putchar('0' + n % 10);
 		}
 		n++;
 	}

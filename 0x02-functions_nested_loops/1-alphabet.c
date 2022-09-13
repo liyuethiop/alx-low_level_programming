8d21806443ed06c65b6c35c3ef3409bfc6c0ed3f
liyu <liyuethiop@gmail.com>
2022-09-13 07:24:27 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - entry
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+}

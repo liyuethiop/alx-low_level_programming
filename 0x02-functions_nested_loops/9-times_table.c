697d4d8ee3243e35a10c1211b138c2b0950032c4
liyu <liyuethiop@gmail.com>
2022-09-13 12:33:18 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,16 +14,13 @@ void times_table(void)
 		for (y = 0; y <= 9; y++)
 		{
 			z = x * y;
-
-			if (y == 0)
-				_putchar('0' + z);
 			if (z / 10 == 0)
 			{
+				_putchar('0' + z % 10);
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar('0' + z % 10);
-							}
+			}
 			else
 			{
 				_putchar(',');

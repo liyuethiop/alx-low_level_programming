c58f6f8516946dd0e1e83100cf5b3cf6cb303b3f
liyu <liyuethiop@gmail.com>
2022-09-13 09:03:01 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - is the fun
+ * @x: is the para
+ * Return: 0
+ */
+int print_last_digit(int x)
+{
+	int y;
+
+	y = x % 10;
+	_putchar('y');
+	return (0);
+}

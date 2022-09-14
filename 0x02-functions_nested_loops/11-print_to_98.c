0102b0155c9e9661857208c9c8308e22bbe92f72
liyu <liyuethiop@gmail.com>
2022-09-14 01:44:05 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,16 +7,17 @@
  */
 void print_to_98(int n)
 {
-	if (n == 98)
-		printf("%i", n);
 	while (n < 98)
 	{
-		printf("%i , ", n);
+		printf("%i, ", n);
 		n++;
 	}
 	while (n > 98)
 	{
-		printf("%i , ", n);
+		printf("%i, ", n);
 		n--;
 	}
+	if (n == 98)
+	printf("%i", n);
+
 }

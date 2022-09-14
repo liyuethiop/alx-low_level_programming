7836cb9aac1f07b2191d517e17a168c7cb54719f
liyu <liyuethiop@gmail.com>
2022-09-14 14:13:15 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	int x, sum;
+
+	for (x = 0; x < 1024; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+			sum += x;
+	}
+	printf("%i\n", sum);
+	return (0);
+}

dd55db58722f36af11e9cf06d1a7d224e27f3cd7
liyu <liyuethiop@gmail.com>
2022-09-14 13:20:36 -0700

    100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * print_times_table - is the fun
+ * @n: is the para
+ * Return: the table
+ */
+void print_times_table(int n)
+{
+	int x;
+	int y;
+	int z;
+
+	if (n <= 15 && n >= 0)
+	{
+
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= x; y++)
+			{
+				z = x * y;
+				if (y == 0)
+					_putchar('0' + z);
+				else if (z / 10 == 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + z % 10);
+
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + z / 10);
+					_putchar('0' + z % 10);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}

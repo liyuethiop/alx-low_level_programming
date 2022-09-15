66f8cfc3bcbaeeb40dcfdafd47f5add4dee6c42b
liyu <liyuethiop@gmail.com>
2022-09-15 12:42:06 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers from 0-14
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 14; i++)
+	{
+		for (j = 0; j < 10; j++)
+			_putchar('0' + i);
+	}
+	_putchar('\n');
+}

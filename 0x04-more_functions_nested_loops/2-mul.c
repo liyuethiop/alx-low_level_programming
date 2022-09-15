a03cfd233181ff662ae70907ab312fcd639b35da
liyu <liyuethiop@gmail.com>
2022-09-15 12:10:15 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integer
+ * @a: is hte parameter
+ * @b: is the 2nd parameter
+ * Return: the multiple
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

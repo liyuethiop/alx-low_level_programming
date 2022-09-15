bca7493c5dd1f1a3ae1307fc928ae1cb59272616
liyu <liyuethiop@gmail.com>
2022-09-15 07:57:54 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,9 +11,10 @@ void print_diagonal(int n)
 	{
 		for (i = 1; i <= n; i++)
 		{
-			_putchar("%c\n", '\');
-			for (j = 1; j <= i; j++)
+			for (j = 0; j <= i; j++)
 				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
 		}
 	}
 	else
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * Return: x
+ */
+
+int _putchar(char x)
+
+{
+
+		return (write(1, &x, 1));
+
+}

f6cc452fa337b4d278f210c2a85e6735d738e8eb
liyu <liyuethiop@gmail.com>
2022-09-20 11:55:44 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - will return the length of a string
+ * @s: is the parameter
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while ( *s != '\0')
+	{
+		*s++;
+		len++;
+	}
+	return len;
+}

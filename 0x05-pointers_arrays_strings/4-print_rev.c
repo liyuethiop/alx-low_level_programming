5944ba93158208deb687f1279c41a51b074b9af8
liyu <liyuethiop@gmail.com>
2022-09-21 02:13:33 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - will print the reverse of a string
+ * @s: is the parameter
+ */
+void print_rev(char *s)
+{
+	int i, len, temp;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}

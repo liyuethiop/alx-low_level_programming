5ab1d0bb2170b50f1f349e277845b0847107ef04
liyu <liyuethiop@gmail.com>
2022-09-21 03:39:44 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - will reverse a string
+ * @s: is the parameter
+ */
+void rev_string(char *s)
+{
+	int i, len;
+	char temp;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}

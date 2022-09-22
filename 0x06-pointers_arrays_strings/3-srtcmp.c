c8bf8c9654c8b3de068026f63821ec082f64f470
liyu <liyuethiop@gmail.com>
2022-09-22 15:26:11 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-srtcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcmp - will compare two strings
+ * @s1: is the first string to compare and
+ * @s2:is the second
+ * Returns: 0 if they are similar
+ * <0 and >0 if one is greater
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (0);
+}

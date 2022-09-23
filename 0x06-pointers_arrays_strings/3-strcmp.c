1b2a5a9ba89001fbf7f73c792ac5ebb6d6bf616e
liyu <liyuethiop@gmail.com>
2022-09-23 05:45:39 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
  * _strcmp - will compare two strings
  * @s1: is the first string to compare and
  * @s2:is the second
- * Returns: 0 if they are similar
+ * Return: 0 if they are similar
  * <0 and >0 if one is greater
  */
 int _strcmp(char *s1, char *s2)

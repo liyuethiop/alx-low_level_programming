24efb3ecdac8614d78eb812341082b7c02704eb8
liyu <liyuethiop@gmail.com>
2022-09-23 06:21:37 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - will change the string to uppercase
+ * @ch: is the string to be changed
+ * Return: the uppercase
+ */
+char *string_toupper(char *ch)
+{
+	int i;
+
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		if (ch[i] <= 'a' && ch[i] <= 'z')
+			ch[i] -= 32;
+	}
+	return (ch);
+}

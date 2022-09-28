99592aabeae4e9637593b97a04637c610eb221fb
liyu <liyuethiop@gmail.com>
2022-09-28 06:22:55 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts_recursion - will print a string using recursion
+ * @s: is the parameter
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+		_putchar(s);
+}
---
+++ 0x08-recursion/__putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ *
+ *  *Return: x
+ *
+ *   */
+
+int _putchar(char x)
+
+{
+
+		return (write(1, &x, 1));
+
+}

bf6a2529cb1facc7747b860a2d4718c3a70f71d7
liyu <liyuethiop@gmail.com>
2022-09-28 07:49:13 -0700

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - will print a string in reverse
+ * @s: is the parameter
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	s--;
+	_putchar(*s);
+	s--;
+	_print_rev_recursion(s);
+}

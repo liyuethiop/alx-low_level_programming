43e206154e1eed1fc8f27f6187fde64870ef9b5e
liyu <liyuethiop@gmail.com>
2022-10-04 06:22:52 -0700

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * _strlen - will return the length of the string
- * _strdup - will return a pointer to a newly allocated space in memory
+ *_strdup - will return a pointer to a newly allocated space in memory
  * @str: is the string to be copied
  * Return:pointer to the duplicated string
  */
@@ -11,7 +11,10 @@ int _strlen(char *str)
 	int i, len = 0;
 
 	for (i = 0; *str != '\0'; i++)
+	{
 		len++;
+		str++;
+	}
 	return (len);
 }
 char *_strdup(char *str)

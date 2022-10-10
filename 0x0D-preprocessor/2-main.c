14e61dd9c76eb797e8777cdfae502b85a7d3e98c
liyu <liyuethiop@gmail.com>
2022-10-10 13:02:38 -0700

    1

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -5,9 +5,9 @@
  * @argv: 2nd parameter
  * Return:0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
-		printf("%s", argv[0]);
+		printf("%s\n", argv[0]);
 	return (0);
 }

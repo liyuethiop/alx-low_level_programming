194777794698512b7e1f7797664ee01dbddec706
liyu <liyuethiop@gmail.com>
2022-10-10 11:52:11 -0700

    2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,10 +3,9 @@
  * main - prints the name of the file it was compiled from
  * @argc:1st parameter
  * @argv: 2nd parameter
- * Return:0
  */
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
-	_putchar(argv[0]);
+	printf ("%s", argv[0]);
 	return (0);
 }

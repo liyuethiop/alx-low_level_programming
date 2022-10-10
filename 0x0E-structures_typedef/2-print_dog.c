a06ffa82a64f7905662ba15ea6c5f2ad88a5137c
liyu <liyuethiop@gmail.com>
2022-10-10 02:45:41 -0700

    2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print a struct dog
+ * @d: variable of struct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	printf("Name: %s\n, Age: %f\n, Owner: %s", d.name, d.age, d.owner);
+}

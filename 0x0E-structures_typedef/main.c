caab89127275bafced02586066bd4d159b480bb8
liyu <liyuethiop@gmail.com>
2022-10-10 13:22:26 -0700

    2

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -18,14 +18,17 @@ int main(void)
 
 {
 
-	    dog_t *my_dog;
+	    struct dog my_dog;
 
 
 
-	        my_dog = new_dog("Poppy", 3.5, "Bob");
 
-		    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+		    my_dog.age = 3.5;
 
-		        return (0);
+		        my_dog.owner = "Bob";
+
+			    print_dog(&my_dog);
+
+			        return (0);
 
 }

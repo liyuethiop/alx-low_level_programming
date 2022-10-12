681c7b884a488aaa1777cfd174785bb72ad3c33c
liyu <liyuethiop@gmail.com>
2022-10-12 10:01:18 -0700

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - entry point
+ * Return: the result
+ */
+int main(void)
+{
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - perform addition
+ * @a:the first number
+ * @b: the second number
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_add - perform subtraction
+ * @a:the first number
+ * @b: the second number
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_add - perform multiplication
+ * @a:the first number
+ * @b: the second number
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_add - perform division
+ * @a:the first number
+ * @b: the second number
+ * Return: a/b
+ */
+int op_div(int a, int b)
+{
+	return(a / b);
+}
+/**
+ * op_add - perform modulo
+ * @a:the first number
+ * @b: the second number
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b)
+}

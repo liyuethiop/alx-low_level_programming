4bc246b39df4616c9d9565a87133649df5ff6d83
liyu <liyuethiop@gmail.com>
2022-10-13 02:18:50 -0700

    0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,18 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - returns sum
+ * @n: fixed parameter
+ * Return:sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum = 0;
+	va_list p;
+
+	va_start(p, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(p, int);
+	va_end(p);
+	return (sum);
+}

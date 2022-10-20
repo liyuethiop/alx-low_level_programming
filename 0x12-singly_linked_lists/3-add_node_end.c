cdd6b0c507cd16cc60d70781fe3a17d868281b4d
liyu <liyuethiop@gmail.com>
2022-10-20 09:10:00 -0700

    3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,10 +11,11 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new, *p;
 	int len = 0;
 
 	new = malloc(sizeof(list_t));
+	p = *head;
 	if (new == NULL)
 		return (NULL);
 	while (str[len] != '\0')
@@ -24,6 +25,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->next = NULL;
 	if (*head == NULL)
 		*head = new;
+	else
+	{
+		while (p->next != NULL)
+			p = p->next;
+		p->next = new;
+	}
 
 	return (new);
 }

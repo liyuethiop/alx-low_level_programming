3f528f869c6e1e2581a54ee70c18d2b6dcc1a59d
liyu <liyuethiop@gmail.com>
2022-10-20 12:32:36 -0700

    101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,25 @@
+global    main
+
+		extern    printf
+
+
+
+	          section   .text
+
+main:	push      rbx	; Call stack must be aligned
+
+	          lea       rdi, [rel message] ; First argument is address of message
+
+		  call      printf	       ; (message)
+
+		  xor 	    eax,eax
+
+		  pop       rbx		       ; Fix up stack before returning
+
+	          ret
+
+
+
+	          section   .data
+
+message:	  db        "Hello, Holberton", 10, 0

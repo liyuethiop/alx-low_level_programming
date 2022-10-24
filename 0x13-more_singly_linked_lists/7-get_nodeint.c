f092b2eb37fc9640c4e1daddd4197246b3acad13
liyu <liyuethiop@gmail.com>
2022-10-24 11:38:12 -0700

    7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - return nth node of list
+ * @head:head
+ * @index:the nth node
+ * Return: the nth node of the list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *t = head;
+	int i;
+
+	if (t)
+	{
+		for (i = 0; i < index; i++)
+			t = t->next;
+			return (t);
+	}
+	else
+		return (NULL);
+}

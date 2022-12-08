f0fb5d40696fd5804b9a2b6970b5e09453616d37
liyu <liyuethiop@gmail.com>
2022-12-08 01:58:12 -0800

    task0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints doubly linked list
+ * @h: head
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t *temp = malloc(sizeof(dlistint_t));
+	int no = 0;
+
+	temp = h;
+	while (temp != NULL)
+	{
+		no++;
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+	return (no);
+}

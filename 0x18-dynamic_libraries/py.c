a6df8789380d5a91d31e4f296e42c1fe7174e913
liyu <liyuethiop@gmail.com>
2022-12-16 02:41:35 -0800

    advansed

---
+++ 0x18-dynamic_libraries/py.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/**
+ * add - function
+ * @a:parameter
+ * @b:parameter
+ * Return:result
+ */
+int add(int a, int b)
+{
+	return(a + b);
+}
+
+/**
+ * sub - function
+ * @a:parameter
+ * @b:parameter
+ * Return:result
+ */
+int sub(int a, int b)
+{
+	return(a - b);
+}
+
+/**
+ * mul - function
+ * @a:parameter
+ * @b:parameter
+ * Return:result
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
+
+/**
+ * div - function
+ * @a:parameter
+ * @b:parameter
+ * Return:result
+ */
+int div(int a, int b)
+{
+	return(a / b);
+}
+
+/**
+ * mod - function
+ * @a:parameter
+ * @b:parameter
+ * Return:result
+ */
+int mod(int a, int b)
+{
+	return(a % b);
+}
